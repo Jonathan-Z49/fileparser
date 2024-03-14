8f1648d0a4a13b1e65068aa900646fa4e0902920
Jonathan Zarate <jonathanz49.dev@gmail.com>
2024-03-14 15:59:23 -0500

    Refactored our general model for parsed data records

--- src/main/java/com/example/backend/models/Flatdata.java
+++ src/main/java/com/example/backend/models/Flatdata.java
@@ -1,7 +1,6 @@
 package com.example.backend.models;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
 import org.springframework.data.mongodb.core.mapping.Field;
@@ -10,26 +9,24 @@ import java.util.List;
 import java.util.Map;
 
 @Document(collection = "flatdata")
-public class FileData {
+public class Flatdata {
     @Id
     private String id;
+    @Field("data")
+    private Map<String, String> data;
 
     @DocumentReference
-    @Field("file")
-    private String fileIdRef;
+    @Field("fileMetaData")
+    private Metadata metadata;
 
-    @Field("data")
-    private List<Map<String, String>> data;
-
-    public FileData() {
+    public Flatdata() {
 
     }
 
-    public FileData(String id, String fileIdRef, List<Map<String, String>> data) {
-        super();
+    public Flatdata(String id, Map<String, String> data ,Metadata metadata) {
         this.id = id;
-        this.fileIdRef = fileIdRef;
         this.data = data;
+        this.metadata = metadata;
     }
 
     public String getId() {
@@ -40,19 +37,27 @@ public class FileData {
         this.id = id;
     }
 
-    public String getFileIdRef() {
-        return fileIdRef;
-    }
-
-    public void setFileIdRef(String fileIdRef) {
-        this.fileIdRef = fileIdRef;
-    }
-
-    public List<Map<String, String>> getData() {
+    public Map<String, String> getData() {
         return data;
     }
 
-    public void setData(List<Map<String, String>> data) {
+    public void setData(Map<String, String> data) {
         this.data = data;
+    }
+
+    public Metadata getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(Metadata metadata) {
+        this.metadata = metadata;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "id='" + id + '\'' +
+                ", data=" + data +
+                '}';
     }
 }
