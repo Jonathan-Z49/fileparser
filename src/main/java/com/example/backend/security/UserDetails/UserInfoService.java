8c1ffad3ae264aa102ee67f7159f7f6f819749f8
Jonathan Zarate <jonathanz49.dev@gmail.com>
2024-03-19 14:17:02 -0500

    Created UserDetailsService impl for user auth

---
+++ src/main/java/com/example/backend/security/UserDetails/UserInfoService.java
@@ -0,0 +1,26 @@
+package com.example.backend.security.UserDetails;
+
+import com.example.backend.models.User;
+import com.example.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserInfoService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserInfoService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+        return UserInfo.build(user);
+    }
+}
