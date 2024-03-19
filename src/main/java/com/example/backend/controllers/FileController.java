4abaffb64daac473169c2b0f694a6851b3ea9aaf
Jonathan Zarate <jonathanz49.dev@gmail.com>
2024-03-19 13:58:13 -0500

    added route for testing JWT token auth, renamed base mapping. WIP

--- src/main/java/com/example/backend/controllers/FileController.java
+++ src/main/java/com/example/backend/controllers/FileController.java
@@ -1,17 +1,21 @@
 package com.example.backend.controllers;
 
+import com.example.backend.models.User;
+import com.example.backend.security.UserDetails.UserInfo;
 import com.example.backend.services.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.security.Principal;
 
 @RestController
+@RequestMapping("api/v1/files")
 public class FileController {
 
     private final FileService fileService;
@@ -21,10 +25,19 @@ public class FileController {
         this.fileService = fileService;
     }
 
-    @PostMapping("/file")
+    @PostMapping("/upload")
     ResponseEntity<String> uploadFile(@RequestParam("specFile") MultipartFile specFile, @RequestParam("dataFile") MultipartFile dataFile) throws IOException {
         String data = fileService.parseFiles(specFile, dataFile);
         return ResponseEntity.ok(data);
+    }
+
+    @GetMapping("/test")
+    ResponseEntity<String> test(Authentication user) {
+        Object prip = user.getPrincipal();
+        System.out.println( ((UserInfo)prip).getAuthorities() );
+        System.out.println( ((UserInfo)prip).getId() );
+        System.out.println( ((UserInfo)prip).getUsername() );
+        return ResponseEntity.ok("TESTED");
     }
 
 }
