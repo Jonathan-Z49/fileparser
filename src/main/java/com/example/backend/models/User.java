0016dc2d95bfed54fd72446251f0f0be544921ad
Jonathan Zarate <jonathanz49.dev@gmail.com>
2024-03-19 14:02:12 -0500

    Added user model for DB persistence after registration through auth controller

---
+++ src/main/java/com/example/backend/models/User.java
@@ -0,0 +1,80 @@
+package com.example.backend.models;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Document(collection = "users")
+public class User {
+
+    @Id
+    private String id;
+
+    @NotBlank
+    @Size(max = 20)
+    @Indexed
+    private String username;
+
+    @NotBlank
+    @Size(max = 50)
+
+    private String password;
+
+    @DBRef
+    private Set<UserRoles> roles = new HashSet<>();
+
+    public User() {
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<UserRoles> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<UserRoles> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "id='" + id + '\'' +
+                ", username='" + username + '\'' +
+                ", roles=" + roles +
+                '}';
+    }
+}
