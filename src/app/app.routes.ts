0019569305b1738bf777c17c84a7f385c84407f7
Jonathan Zarate <jonathanz49.dev@gmail.com>
2024-03-28 17:35:54 -0500

    added file route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { FileuploadComponent } from './fileupload/fileupload.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'login', pathMatch: 'full'},
     {path: 'login', component: LoginComponent},
-    {path: 'register', component: LoginComponent}
+    {path: 'register', component: LoginComponent},
+    {path: 'files', component: FileuploadComponent}
 ];
