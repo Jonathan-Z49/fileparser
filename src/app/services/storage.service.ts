1b210d2468f58665c1c0330a663696b2847e22a0
Jonathan Zarate <jonathanz49.dev@gmail.com>
2024-03-28 17:37:14 -0500

    Added localstorage service for adding user info

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { JWTRes } from '../interfaces/JWTres';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,23 @@ import { Injectable } from '@angular/core';
 export class StorageService {
 
   constructor() { }
+
+  clear() {
+    window.sessionStorage.clear();
+  }
+
+  setUser(user: JWTRes) {
+    this.clear();
+    window.sessionStorage.setItem("user", JSON.stringify(user));
+  }
+
+  getUser(): JWTRes | null {
+    const user = window.sessionStorage.getItem("user");
+
+    if(user) {
+      return JSON.parse(user);
+    }
+    return null;
+  }
+
 }
