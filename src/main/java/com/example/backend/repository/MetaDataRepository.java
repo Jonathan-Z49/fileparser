97efa95b5d16c38a45438db15378c1a2f1ba0e47
Jonathan Zarate <jonathanz49.dev@gmail.com>
2024-04-03 16:54:49 -0500

    Added query method to search by type

--- src/main/java/com/example/backend/repository/MetaDataRepository.java
+++ src/main/java/com/example/backend/repository/MetaDataRepository.java
@@ -1,10 +1,14 @@
 package com.example.backend.repository;
 
 import com.example.backend.models.Metadata;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface MetaDataRepository extends MongoRepository<Metadata, String> {
-
+    Optional<List<Metadata>> findAllByUserAndTypeOrderByCreatedAtDesc(ObjectId userId, String type);
 }
