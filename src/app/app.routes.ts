89dc424db389a772b2a24f7d537b37cfb14a588b
Jonathan Zarate <jonathanz49.dev@gmail.com>
2024-04-03 17:02:39 -0500

    added new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { FileuploadComponent } from './fileupload/fileupload.component';
 import { FileListComponent } from './file-list/file-list.component';
+import { RecordsComponent } from './records/records.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'login', pathMatch: 'full'},
@@ -9,4 +10,6 @@ export const routes: Routes = [
     {path: 'register', component: LoginComponent},
     {path: 'files', component: FileuploadComponent},
     {path: 'files/flat', component: FileListComponent},
+    {path: 'files/spec', component: FileListComponent},
+    {path: 'files/:id/records', component: RecordsComponent},
 ];
