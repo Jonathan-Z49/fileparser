0433ab0cdd71032e1b69ccda978caacfd8fe31ab
Jonathan Zarate <jonathanz49.dev@gmail.com>
2024-04-03 16:48:53 -0500

    S3 bucket file download impl

---
+++ src/main/java/com/example/backend/s3/S3FileService.java
@@ -0,0 +1,67 @@
+package com.example.backend.s3;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Service
+public class S3FileService {
+    @Value("${aws.bucket.name}")
+    private String bucketName;
+
+    private final AmazonS3 s3Client;
+
+    @Autowired
+    public S3FileService(AmazonS3 s3Client){
+        this.s3Client = s3Client;
+    }
+
+    public void uploadFile(String metaDataId, MultipartFile multipartFile) throws IOException {
+
+        File file = new File(multipartFile.getOriginalFilename());
+        try (FileOutputStream fileOutputStream = new FileOutputStream(file)){
+            fileOutputStream.write(multipartFile.getBytes());
+        }
+
+        PutObjectRequest request = new PutObjectRequest(bucketName, metaDataId, file);
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.addUserMetadata("Title", "File Upload - " + metadata);
+        metadata.setContentLength(file.length());
+        request.setMetadata(metadata);
+        s3Client.putObject(request);
+
+        file.delete();
+    }
+
+    public Object downloadFile(String fileName) throws IOException {
+
+        S3Object object = s3Client.getObject(bucketName, fileName);
+        try (S3ObjectInputStream s3is = object.getObjectContent()) {
+            try (FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
+                byte[] read_buf = new byte[1024];
+                int read_len = 0;
+                while ((read_len = s3is.read(read_buf)) > 0) {
+                    fileOutputStream.write(read_buf, 0, read_len);
+                }
+            }
+            Path pathObject = Paths.get(fileName);
+            Resource resource = new UrlResource(pathObject.toUri());
+
+            if (resource.exists() || resource.isReadable()) {
+                return resource;
+            }
+        }
+        return null;
+    }
+}
