2d6cb54e1b388b35b44d316d672e797bb6df588d
Jonathan Zarate <jonathanz49.dev@gmail.com>
2024-04-03 16:59:35 -0500

    refactoring, added method for querying by file

--- src/main/java/com/example/backend/services/FlatDataService.java
+++ src/main/java/com/example/backend/services/FlatDataService.java
@@ -3,12 +3,14 @@ package com.example.backend.services;
 import com.example.backend.models.Flatdata;
 import com.example.backend.models.Metadata;
 import com.example.backend.repository.FlatDataRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class FlatDataService {
@@ -20,15 +22,16 @@ public class FlatDataService {
         this.flatDataRepository = flatDataRepository;
     }
 
-    public List<Flatdata> createFlatData(List<Map<String, String>> records, Metadata file){
+    public List<Flatdata> createFlatData(List<Map<String, String>> records, Metadata flatFile, Metadata specFile, String userId){
         List<Flatdata> allRecords = new ArrayList<>();
         records.forEach(recordMap -> {
-            Flatdata dataRecord = new Flatdata();
-            dataRecord.setData(recordMap);
-            dataRecord.setMetadata(file);
+            Flatdata dataRecord = new Flatdata(recordMap, flatFile, specFile, new ObjectId(userId));
             allRecords.add(flatDataRepository.save(dataRecord));
         });
-
         return allRecords;
+    }
+
+    public List<Flatdata> findFlatDataByFile(ObjectId id, ObjectId userId){
+        return flatDataRepository.findAllByFileId(id, userId).orElse(null);
     }
 }
