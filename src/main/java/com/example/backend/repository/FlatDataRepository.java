2257470b488d683ad837813ec67a840a2842439b
Jonathan Zarate <jonathanz49.dev@gmail.com>
2024-04-03 16:53:30 -0500

    Added query method for searching for records based on a metadata record

--- src/main/java/com/example/backend/repository/FlatDataRepository.java
+++ src/main/java/com/example/backend/repository/FlatDataRepository.java
@@ -1,8 +1,19 @@
 package com.example.backend.repository;
 
 import com.example.backend.models.Flatdata;
+import com.example.backend.models.Metadata;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface FlatDataRepository extends MongoRepository<Flatdata, String> {
+
+    @Query("{ $or: [ { 'fileFlatData': ?0 }, { 'fileSpecData': ?0 } ], 'user': ?1 }")
+    Optional<List<Flatdata>> findAllByFileId(ObjectId metadataId, ObjectId user);
 
 }
