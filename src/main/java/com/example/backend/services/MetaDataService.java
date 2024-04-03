252a60324528a5f2cb68694ad54e1cfbd4735e9f
Jonathan Zarate <jonathanz49.dev@gmail.com>
2024-04-03 17:00:52 -0500

    added new method to find by user id, refactoring

--- src/main/java/com/example/backend/services/MetaDataService.java
+++ src/main/java/com/example/backend/services/MetaDataService.java
@@ -1,10 +1,14 @@
 package com.example.backend.services;
 
 import com.example.backend.models.Metadata;
+import com.example.backend.models.User;
 import com.example.backend.repository.MetaDataRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Service
 public class MetaDataService {
@@ -16,8 +20,12 @@ public class MetaDataService {
         this.metaDataRepository = metaDataRepository;
     }
 
-    public Metadata createFileMetaData(MultipartFile file) {
-        Metadata metadata = new Metadata(file.getSize(), file.getOriginalFilename());
+    public Metadata createFileMetaData(MultipartFile file, String userId, String type) {
+        Metadata metadata = new Metadata(file.getSize(), file.getOriginalFilename(), new ObjectId(userId), type);
         return metaDataRepository.save(metadata);
+    }
+
+    public List<Metadata> findByUserIdAndType(ObjectId userId, String type) {
+        return metaDataRepository.findAllByUserAndTypeOrderByCreatedAtDesc(userId, type).orElse(null);
     }
 }
