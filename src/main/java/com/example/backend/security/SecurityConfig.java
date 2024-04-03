394dc9d8edc46ff943e56ced57ec36359e4b75a6
Jonathan Zarate <jonathanz49.dev@gmail.com>
2024-04-03 16:58:40 -0500

    changed permitted routes

--- src/main/java/com/example/backend/security/SecurityConfig.java
+++ src/main/java/com/example/backend/security/SecurityConfig.java
@@ -57,8 +57,7 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable())
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1/auth/**").permitAll().requestMatchers("/api/v1/test/**")
-                        .permitAll().anyRequest().authenticated());
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1/auth/**").permitAll().anyRequest().authenticated());
 
         http.authenticationProvider(authenticationProvider());
 
