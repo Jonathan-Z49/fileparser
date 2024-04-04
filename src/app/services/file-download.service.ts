04121023bcf2651fe3f8d034d37295bdcb93f1f5
Jonathan Zarate <jonathanz49.dev@gmail.com>
2024-04-04 14:05:02 -0500

    added ability for users to download files from our server

---
+++ src/app/services/file-download.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { StorageService } from './storage.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileDownloadService {
+
+  constructor(private http: HttpClient, private storageService: StorageService) { }
+
+  downloadFile(name: string, id: string): Observable<HttpResponse<Blob>> {
+
+    const headers = new HttpHeaders()
+      .set('Authorization', 'Bearer ' + this.storageService.getUser()?.accessToken);
+
+    return this.http.get(`http://localhost:8080/api/v1/files/${name}/${id}` , {
+      headers,
+      observe: 'response',
+      responseType: 'blob'
+    });
+  }
+}
